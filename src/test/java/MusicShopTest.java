aca3c51c3cf6780df83b61bc4781b682a6f56dbd
Bela <belahamid@gmail.com>
2021-02-21 21:24:00 +0000

    completed set-up in music shop test file

--- src/test/java/MusicShopTest.java
+++ src/test/java/MusicShopTest.java
@@ -1,5 +1,5 @@
 import behaviours.ISell;
-import items.Item;
+import items.*;
 import org.junit.Before;
 import org.junit.Test;
 import shop.MusicShop;
@@ -11,15 +11,35 @@ import static org.junit.Assert.assertEquals;
 public class MusicShopTest {
     String name;
     MusicShop musicShop;
+    ArrayList<ISell> stock;
+    Piano piano;
+    Guitar guitar;
+    SheetMusic sheetMusic;
+    GuitarStrings guitarStrings;
 
 
     @Before
     public void before(){
-        musicShop = new MusicShop("Max's music store");
+        musicShop = new MusicShop("Max's music shop");
+        stock = new ArrayList<ISell>();
+        piano = new Piano(
+                "Grand Piano", 500.00, 1000.00,
+                "black", "maple", 3
+        );
+        guitar = new Guitar(
+                "Classical Guitar", 30, 100,
+                "brown", "spruce", 6
+        );
+        sheetMusic = new SheetMusic(
+                "Piano score", 1.00, 7.00
+        );
+        guitarStrings = new GuitarStrings(
+                "B string", 5.00, 20.00
+        );
     }
 
     @Test
     public void storeHasName(){
-        assertEquals("Max's music store", musicShop.getName());
+        assertEquals("Max's music shop", musicShop.getName());
     }
 }
