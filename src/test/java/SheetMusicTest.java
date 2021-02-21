712eae2dfd7f77e2928f4fa232086d799aff978b
Bela <belahamid@gmail.com>
2021-02-21 13:59:29 +0000

    sheet music has buying and selling price

--- src/test/java/SheetMusicTest.java
+++ src/test/java/SheetMusicTest.java
@@ -16,4 +16,15 @@ public class SheetMusicTest {
     public void hasItemName(){
         assertEquals("Piano score", sheetMusic.getItemName());
     }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(1.00, sheetMusic.getBuyingPrice(), 0.00);
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(7.00, sheetMusic.getSellingPrice(), 0.00);
+    }
+
 }
