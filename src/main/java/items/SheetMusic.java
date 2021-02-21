0ec18f99c888572fed216aba6ce833645db7a0fe
Bela <belahamid@gmail.com>
2021-02-21 13:57:52 +0000

    test passed: sheetmusic has itemname

--- src/main/java/items/SheetMusic.java
+++ src/main/java/items/SheetMusic.java
@@ -1,4 +1,9 @@
 package items;
 
-public class SheetMusic {
+public class SheetMusic extends Item{
+
+    public SheetMusic(String itemName, double buyingPrice, double sellingPrice){
+        super(itemName, buyingPrice, sellingPrice);
+    }
+
 }
--- src/test/java/SheetMusicTest.java
+++ src/test/java/SheetMusicTest.java
@@ -1,2 +1,19 @@
+import items.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SheetMusicTest {
+    SheetMusic sheetMusic;
+
+    @Before
+    public void before(){
+        sheetMusic = new SheetMusic("Piano score", 1.00, 7.00);
+    }
+
+    @Test
+    public void hasItemName(){
+        assertEquals("Piano score", sheetMusic.getItemName());
+    }
 }
